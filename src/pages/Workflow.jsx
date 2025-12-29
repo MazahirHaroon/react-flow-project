03d996ff927276b6c78bb6f1e67b84de74b13f60
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-30 00:26:08 +0530

    feat: add edge creation on node connect in workflow

--- src/pages/Workflow.jsx
+++ src/pages/Workflow.jsx
@@ -1,9 +1,12 @@
+import { useCallback } from 'react';
+
 import {
   ReactFlow,
   Background,
   Controls,
   useNodesState,
   useEdgesState,
+  addEdge,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
@@ -22,6 +25,13 @@ const initialNodes = [
     },
     position: { x: 200, y: 200 },
   },
+  {
+    id: '3',
+    data: {
+      label: 'Node 3',
+    },
+    position: { x: 300, y: 400 },
+  },
 ];
 
 const initialEdges = [
@@ -37,6 +47,22 @@ const Workflow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+  const onConnect = useCallback(
+    (connection) => {
+      setEdges((prevEdges) =>
+        addEdge(
+          {
+            ...connection,
+            animated: true,
+            id: `${prevEdges.length + 1}`,
+          },
+          prevEdges
+        )
+      );
+    },
+    [setEdges]
+  );
+
   return (
     <div className='h-screen w-screen border-2 border-red-400'>
       <ReactFlow
@@ -44,6 +70,7 @@ const Workflow = () => {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
         fitView
       >
         <Background />
